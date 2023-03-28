e6ccdb85d03968adaa567acb2bdef466182b3685
Hipreme <msnmancini@hotmail.com>
2023-03-28 15:43:53 -0300

    Added game source

--- source/gamescript/piece.d
+++ source/gamescript/piece.d
@@ -1,19 +1,23 @@
-module script.piece;
+module gamescript.piece;
 import hip.api;
 
 class Piece
 {
     float x, y;
+    float scaleX, scaleY;
     int type;
     ubyte gridX, gridY;
     IHipTextureRegion region;
 
-    this(int type, IHipTextureRegion region, float startX, float startY, ubyte gridX, ubyte gridY)
+    this(int type, IHipTextureRegion region, float startX, float startY, ubyte gridX, ubyte gridY,
+    float scaleX, float scaleY)
     {
         this.type = type;
         this.region = region;
         x = startX;
         y = startY;
+        this.scaleX = scaleX;
+        this.scaleY = scaleY;
         this.gridX = gridX;
         this.gridY = gridY;
     }
@@ -25,9 +29,9 @@ class Piece
         swap(gridY, piece.gridY);
     }
 
-    void draw()
+    void draw(int xOffset, int yOffset)
     {
         if(region !is null)
-            drawRegion(region, cast(int)x, cast(int)y);
+            drawRegion(region, cast(int)x+xOffset, cast(int)y+yOffset, 0, HipColor.white, scaleX, scaleY);
     }
 }
